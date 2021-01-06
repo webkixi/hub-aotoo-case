d0737edad9e419cf3270bacb6bf02a9d3642529c
yc <kixi@163.com>
2021-01-06 16:09:19 +0800

    优化

---
+++ wp-babel.js
@@ -0,0 +1,16 @@
+/**
+ * aotoo-hub环境中webpack的babel的options配置补充方法
+ * @param {Object} babelConfig babel.options
+ * @param {String} fromdll is babel.options from dll or not
+ */
+module.exports = function(babelConfig, fromdll){
+  // let options = babelConfig.options
+  // let plugins = options.plugins
+  // plugins.concat(
+  //   [
+  //     ...
+  //   ]
+  // )
+  // babelConfig.options.plugins = plugins
+  return babelConfig
+}
---
+++ wp-config.js
@@ -0,0 +1,8 @@
+/**
+ * aotoo-hub环境中webpack配置补充方法
+ * @param {Object} wpconfig config of webpack
+ * @param {String} fromdll is babel.options from dll or not
+ */
+module.exports = function(wpconfig, fromdll){
+  return wpconfig
+}
